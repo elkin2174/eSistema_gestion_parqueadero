69d14e0f58addaa1520e23630f37b11f87c4c4ff
ELKIN-SCHNEDERS <25920378-ELKIN-SCHNEDERS@users.noreply.replit.com>
2024-02-05 00:14:59 +0000

    Implemetacion metodo generar Recibo clase Parqueadero

--- Parqueadero.cpp
+++ Parqueadero.cpp
@@ -1,4 +1,6 @@
 #include "Parqueadero.h"
+#include <string>
+#include <algorithm>
 
 string Parqueadero::getNombre(){
   return this->nombre;
@@ -26,11 +28,35 @@ void Parqueadero::imprimirCantidad(){
 void Parqueadero::generarRecibo(){
   int horaLL = 0, horaSa = 0, minll = 0, minSa = 0;
   string id = "", nom = "", lic = "", pla = "", mar = "", mod = "";
+  string tipo = "";
   cout << "Ingrese los datos del Tiempo: " <<endl;
   cout << "Hora Entrada: "; cin >> horaLL;
-  cout<<endl<< "Minuto entrada: ", cin >> minll;
-  cout<<endl<< "Hora Salida: ", cin >> horaSa;
-  cout<<endl<< "Minuto Salida: ", cin >> minSa;
+  cout<<endl<< "Minuto entrada: "; cin >> minll;
+  cout<<endl<< "Hora Salida: "; cin >> horaSa;
+  cout<<endl<< "Minuto Salida: "; cin >> minSa;
+  cin.ignore();
+  cout<<endl<< "Documeto del Cliente: "; getline(cin,id);
+  cout<<endl<< "Nombre del Cliente: "; getline(cin,nom);
+  cout<<endl<< "licencia del Cliente: "; getline(cin,lic);
+  cout<<endl<< "Placa del Vehiculo: "; getline(cin,pla);
+  cout<<endl<< "Marca del Cliente: "; getline(cin,mar);
+  cout<<endl<< "Modelo del Cliente: "; getline(cin,mod);
+  cout<<endl<< "Tipo de Veiculo"; getline(cin,tipo);
+  transform(tipo.begin(), tipo.end(), tipo.begin(), ::toupper);
+  
+  if(tipo == "AUTOMOVIL"){
+    int pue = 0, pasa = 0;
+    cout<<endl<< "Numero de puertas: "; cin >> pue;
+    cout<<endl<< "Numero de pasajeros: "; cin >> pasa;
+    recibos.push_back(new Recibo(horaLL, horaSa, minll, minSa, id, nom, lic, pla, mar, mod, pue, pasa));
+    
+  }else if(tipo == "MOTOCICLETA"){
+    float cil = 0.0;
+    recibos.push_back(new Recibo(horaLL, horaSa, minll, minSa, id, nom, lic, pla, mar, mod, cil));
+    
+  }else{
+    cout << "Porfavor ingrese un tipo correcto.";
+  }
 
-
+  
 }
